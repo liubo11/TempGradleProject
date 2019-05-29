3a914fcf2d5043dbfbe21fe8ebae0497a7645bf3
liubo <liubo233@qq.com>
2019-05-29 14:14:44 +0800

    init project

--- app/src/main/java/com/lb/collection/MainActivity.java
+++ app/src/main/java/com/lb/collection/MainActivity.java
@@ -1,6 +1,9 @@
 package com.lb.collection;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -15,7 +18,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         TextView tv = new TextView(this);
-        tv.setText("xxxxxxxxxxxxxxxxxxxxxxxxxx");
+        tv.setText("xxxxxxxxxxxxxx"+getAppVersionName());
         setContentView(tv);
+    }
+    /**
+     * 获取版本字符串
+     */
+    public String getAppVersionName() {
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            return packageInfo.versionName+"."+ packageInfo.getLongVersionCode();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "1.0.1";
     }
 }
